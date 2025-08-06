f34a2695ac0aa29ff10ecdee3541ae33aab564e7
User <asqarservis0000@gmail.com>
2025-08-06 21:16:28 +0300

    first commit

--- apps/urls.py
+++ apps/urls.py
@@ -3,10 +3,19 @@ from apps.views import *
 
 urlpatterns = [
     path('register/', RegisterCreateAPIView.as_view()),
-    path('create/event/', EventCreateAPIView.as_view()),
-    path('list/event/', EventListAPIView.as_view()),
-    path('registeration/event', EventRegisterCreateAPIView.as_view()),
-    path('list/user/event/', UserEventListAPIView.as_view()),
     path('login/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', CustomTokenRefreshView.as_view(), name='token_refresh')
+]
+
+urlpatterns += [
+    path('create/event/', EventCreateAPIView.as_view()),
+
+    path('registeration/event/', EventRegisterCreateAPIView.as_view()),
+]
+
+urlpatterns += [
+    path('list/user/event/', UserEventListAPIView.as_view()),
+    path('list/event/', EventListAPIView.as_view()),
+    path('event/<int:pk>/', EventRetrieveAPIView.as_view()),
+    path('user/register/evets/list', RegisterEventListAPIView.as_view()),
 ]
--- apps/views.py
+++ apps/views.py
@@ -1,22 +1,27 @@
-from rest_framework.generics import CreateAPIView, ListAPIView
+from drf_spectacular.utils import extend_schema
+from rest_framework.generics import CreateAPIView, ListAPIView, RetrieveAPIView
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
-from apps.models import Event
+from apps.models import Event, Registration
 from apps.serializer import UserModelSerializer, EventModelSerializer, RegistrationModelSerializer
 
 
+@extend_schema(tags=['login-register'])
 class CustomTokenObtainPairView(TokenObtainPairView):
     pass
 
 
+@extend_schema(tags=['login-register'])
 class CustomTokenRefreshView(TokenRefreshView):
     pass
 
 
+@extend_schema(tags=['login-register'])
 class RegisterCreateAPIView(CreateAPIView):
     serializer_class = UserModelSerializer
 
 
+@extend_schema(tags=['event'])
 class EventCreateAPIView(CreateAPIView):
     serializer_class = EventModelSerializer
 
@@ -24,11 +29,31 @@ class EventCreateAPIView(CreateAPIView):
         serializer.save(organizer_id=self.request.user)
 
 
+@extend_schema(tags=['event'])
 class EventListAPIView(ListAPIView):
     serializer_class = EventModelSerializer
     queryset = Event.objects.all()
 
 
+@extend_schema(tags=['event'])
+class EventRetrieveAPIView(RetrieveAPIView):
+    serializer_class = EventModelSerializer
+    queryset = Event.objects.all()
+    lookup_url_kwarg = 'pk'
+
+
+@extend_schema(tags=['event'])
+class UserEventListAPIView(ListAPIView):
+    queryset = Event.objects.all()
+    serializer_class = EventModelSerializer
+
+    def get_queryset(self):
+        query = super().get_queryset()
+        query = query.filter(organizer_id=self.request.user)
+        return query
+
+
+@extend_schema(tags=['registration'])
 class EventRegisterCreateAPIView(CreateAPIView):
     serializer_class = RegistrationModelSerializer
 
@@ -36,13 +61,12 @@ class EventRegisterCreateAPIView(CreateAPIView):
         serializer.save(user_id=self.request.user)
 
 
-class UserEventListAPIView(ListAPIView):
-    queryset = Event.objects.all()
-    serializer_class = EventModelSerializer
+@extend_schema(tags=['registration'])
+class RegisterEventListAPIView(ListAPIView):
+    serializer_class = RegistrationModelSerializer
+    queryset = Registration.objects.all()
 
     def get_queryset(self):
-        query=super().get_queryset()
-        query=query.filter(organizer_id=self.request.user)
+        query = super().get_queryset()
+        query = query.filter(user_id=self.request.user)
         return query
-
-
